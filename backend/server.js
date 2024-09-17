866d23ac86a9bbd387196402e734e55804330bdb
Sergio Esteban <Sergioss772022@gmail.com>
2024-09-17 21:57:09 +0200

    initial routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
-const express = require("express"); 
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express(); 
 
 
+dotenv.config();
+const PORT = process.env.PORT || 5000; 
+
+
 app.get("/", (req, res) => {
 
-    
-})
+    res.send("hello sergi")
+});
+
+app.use("/api/auth",authRoutes)
 
 
 
-app.listen(5000, () => console.log("Server running on port 5000")); 
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
 
